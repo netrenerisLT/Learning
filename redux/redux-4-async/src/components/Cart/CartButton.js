9e510093cbe48a6076dbeb8ab7ea1ae1e51e1079
Jonas Kazlauskas <netreneris@gmail.com>
2023-02-01 14:32:26 +0200

    init

--- redux/redux-4-async/src/components/Cart/CartButton.js
+++ redux/redux-4-async/src/components/Cart/CartButton.js
@@ -1,17 +1,17 @@
 import classes from "./CartButton.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { uiSliceActions } from "../../store/uiSlice";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
-
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
   const toggleCartHandler = () => {
     dispatch(uiSliceActions.toggle());
   };
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- redux/redux-4-async/src/store/cartSlice.js
+++ redux/redux-4-async/src/store/cartSlice.js
@@ -4,13 +4,14 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: { items: [], totalQuantity: 0, totalPrice: 0 },
   reducers: {
-    addItem(state, action) {
+    addItemToCart(state, action) {
       const newItem = action.payload;
       //cheks if item exists
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -24,6 +25,7 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
