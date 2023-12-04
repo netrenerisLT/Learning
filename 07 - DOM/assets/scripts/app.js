c9457442f4987b6df137cfb25bcecd3bb73e6de7
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-04 20:49:55 +0200

    Update app.js

--- 07 - DOM/assets/scripts/app.js
+++ 07 - DOM/assets/scripts/app.js
@@ -4,8 +4,44 @@ const startAddMovieButton = document.querySelector("header button");
 const backdropBackground = document.getElementById("backdrop");
 const confirmToAddMovie = cancelMovieButton.nextElementSibling;
 const userInputs = addMovieModalButton.querySelectorAll("input");
+const entryTextSectino = document.getElementById("entry-text");
 
 const movies = [];
+
+const deleteMovieHandler = (movieId) => {
+  let identifiedId = 0;
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    identifiedId++;
+  }
+  movies.splice(identifiedId, 1);
+  const listRoot = document.getElementById("movie-list");
+  listRoot.children[identifiedId].remove();
+};
+
+const updateUI = () => {
+  movies.length >= 1 ? (entryTextSectino.style.display = "none") : null;
+};
+
+const addNewMovieElement = (id, title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.className = "movie-element";
+  newMovieElement.innerHTML = `
+  <div class="movie-element__image">
+  <img src="${imageUrl}" alt="${title}" />
+  </div>
+  <div class="movie-element__info">
+  <h2>${title}</h2>
+  <p>${rating}/5 stars</p> 
+  </div>
+  `;
+
+  newMovieElement.addEventListener("click", deleteMovieHandler.bind(null, id));
+  const listRoot = document.getElementById("movie-list");
+  listRoot.append(newMovieElement);
+};
 
 const toggleBackdropHandler = () => {
   backdropBackground.classList.toggle("visible");
@@ -38,6 +74,7 @@ const addMovieToListHandler = () => {
     alert("Please enter a valid values.");
   } else {
     const newMovie = {
+      id: Math.random().toString(),
       title: titleValue,
       image: imgUrlValue,
       rating: ratingValue,
@@ -46,6 +83,13 @@ const addMovieToListHandler = () => {
     console.log(movies);
     clearInputs();
     toggleMovieModalHandler();
+    addNewMovieElement(
+      newMovie.id,
+      newMovie.title,
+      newMovie.image,
+      newMovie.rating
+    );
+    updateUI();
   }
 };
 
