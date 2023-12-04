2e528390fa1896e4956b022e97166e40d5816fc2
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-04 19:46:18 +0200

    working on form input and saving data

--- 07 - DOM/assets/scripts/app.js
+++ 07 - DOM/assets/scripts/app.js
@@ -2,20 +2,54 @@ const addMovieModalButton = document.getElementById("add-modal");
 const cancelMovieButton = addMovieModalButton.querySelector(".btn--passive");
 const startAddMovieButton = document.querySelector("header button");
 const backdropBackground = document.getElementById("backdrop");
+const confirmToAddMovie = cancelMovieButton.nextElementSibling;
+const userInputs = addMovieModalButton.querySelectorAll("input");
 
-const toggleBackdrop = () => {
+const movies = [];
+
+const toggleBackdropHandler = () => {
   backdropBackground.classList.toggle("visible");
 };
 
-const hideAddMovie = () => {
-  toggleMovieModal();
+const clearInputs = () => {
+  for (const input of userInputs) {
+    input.value = "";
+  }
 };
 
-const toggleMovieModal = () => {
+const toggleMovieModalHandler = () => {
   addMovieModalButton.classList.toggle("visible");
-  toggleBackdrop();
+  clearInputs();
+  toggleBackdropHandler();
 };
 
-startAddMovieButton.addEventListener("click", toggleMovieModal);
-backdropBackground.addEventListener("click", hideAddMovie);
-cancelMovieButton.addEventListener("click", hideAddMovie);
+const addMovieToListHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imgUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imgUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please enter a valid values.");
+  } else {
+    const newMovie = {
+      title: titleValue,
+      image: imgUrlValue,
+      rating: ratingValue,
+    };
+    movies.push(newMovie);
+    console.log(movies);
+    clearInputs();
+    toggleMovieModalHandler();
+  }
+};
+
+startAddMovieButton.addEventListener("click", toggleMovieModalHandler);
+backdropBackground.addEventListener("click", toggleMovieModalHandler);
+cancelMovieButton.addEventListener("click", toggleMovieModalHandler);
+confirmToAddMovie.addEventListener("click", addMovieToListHandler);
