af02515bf1dc6d848ed0676e847f662cd8fd2016
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-06 20:10:26 +0200

    class update

--- 09- OOP/assets/scripts/app.js
+++ 09- OOP/assets/scripts/app.js
@@ -1,9 +1,4 @@
-class ProductItem {
-  title = "DEFAULTVALUE";
-  imageUrl;
-  description;
-  price;
-
+class Product {
   constructor(title, image, desc, price) {
     (this.title = title),
       (this.imageUrl = image),
@@ -12,43 +7,58 @@ class ProductItem {
   }
 }
 
-const productList = {
-  products: [
-    new ProductItem(
+class ProductItem {
+  //hold the logic to render single item
+  constructor(product) {
+    this.product = product;
+  }
+  renderItem() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+    <div>
+      <img src="${this.product.imageUrl}" alt="${this.product.title}">
+      <div class="product-item__content">
+      <h2>${this.product.title}</h2>
+      <h3>\$${this.product.price}</h3>
+      <h4>${this.product.description}</h4>
+      <button>Add to Cart</button>
+      </div>
+    </div>
+    `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
       "A pillow",
       "https://hips.hearstapps.com/hmg-prod/images/ghi-best-pillows-1573668641.png?crop=0.621xw:0.953xh;0.194xw,0.0471xh&resize=1200:*",
       "Soft with flowerish smell",
       "19.99"
     ),
-    new ProductItem(
+    new Product(
       "A carpet",
       "https://hips.hearstapps.com/hmg-prod/images/full-frame-shot-of-carpet-royalty-free-image-939098600-1548357822.jpg?crop=1xw:1xh;center,top&resize=980:*",
       "Clean and without dog's hairs",
       "59.99"
     ),
-  ],
+  ];
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-      <div>
-        <img src="${prod.imageUrl}" alt="${prod.title}">
-        <div class="product-item__content">
-        <h2>${prod.title}</h2>
-        <h3>\$${prod.price}</h3>
-        <h4>${prod.description}</h4>
-        <button>Add to Cart</button>
-        </div>
-      </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.renderItem();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
