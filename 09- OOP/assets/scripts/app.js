8aee3a491c1706800b024b3b9adf43718fc5f930
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-06 18:38:22 +0200

    updated product list as classes

--- 09- OOP/assets/scripts/app.js
+++ 09- OOP/assets/scripts/app.js
@@ -1,19 +1,31 @@
+class ProductItem {
+  title = "DEFAULTVALUE";
+  imageUrl;
+  description;
+  price;
+
+  constructor(title, image, desc, price) {
+    (this.title = title),
+      (this.imageUrl = image),
+      (this.description = desc),
+      (this.price = price);
+  }
+}
+
 const productList = {
   products: [
-    {
-      title: "A pillow",
-      imgUrl:
-        "https://cdn.thewirecutter.com/wp-content/media/2023/01/bedpillows-2048px-9999.jpg",
-      price: "19.99",
-      description: "Soft and fresh smelling",
-    },
-    {
-      title: "A carpet",
-      imgUrl:
-        "https://hips.hearstapps.com/hmg-prod/images/full-frame-shot-of-carpet-royalty-free-image-939098600-1548357822.jpg?crop=1xw:1xh;center,top&resize=980:*",
-      price: "49.99",
-      description: "Clean and without dog's hairs",
-    },
+    new ProductItem(
+      "A pillow",
+      "https://hips.hearstapps.com/hmg-prod/images/ghi-best-pillows-1573668641.png?crop=0.621xw:0.953xh;0.194xw,0.0471xh&resize=1200:*",
+      "Soft with flowerish smell",
+      "19.99"
+    ),
+    new ProductItem(
+      "A carpet",
+      "https://hips.hearstapps.com/hmg-prod/images/full-frame-shot-of-carpet-royalty-free-image-939098600-1548357822.jpg?crop=1xw:1xh;center,top&resize=980:*",
+      "Clean and without dog's hairs",
+      "59.99"
+    ),
   ],
   render() {
     const renderHook = document.getElementById("app");
@@ -24,7 +36,7 @@ const productList = {
       prodEl.className = "product-item";
       prodEl.innerHTML = `
       <div>
-        <img src="${prod.imgUrl}" alt="${prod.title}">
+        <img src="${prod.imageUrl}" alt="${prod.title}">
         <div class="product-item__content">
         <h2>${prod.title}</h2>
         <h3>\$${prod.price}</h3>
