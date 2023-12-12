246c96636a11d5968b1526d04a26350a3e965cde
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-12 18:15:40 +0200

    fixed items switch

--- 09.01 - OOP practice/assets/scripts/app.js
+++ 09.01 - OOP practice/assets/scripts/app.js
@@ -1,23 +1,52 @@
+// STEP 6 select list
+class DOMHelper {
+  // STEP 8.2 clean memory leak after the switch
+  static clearEventListeners(element) {
+    const clonedElement = element.cloneNode(true);
+    element.replaceWith(clonedElement);
+    return clonedElement;
+  }
+
+  static moveElement(elementId, newDestinationSelector) {
+    const element = document.getElementById(elementId);
+    const destinationElement = document.querySelector(newDestinationSelector);
+    destinationElement.append(element);
+  }
+}
+
 class Tooltip {}
 
 class ProjectItem {
   //STEP 2. transfer item's id to this class
-  constructor(prjItemsIteratorId, updateProjectListFunction) {
+  constructor(prjItemsIteratorId, updateProjectListFunction, type) {
     this.prjItemsIteratorId = prjItemsIteratorId;
     this.updateProjectListHandler = updateProjectListFunction;
     this.connectMoreInfoButton();
-    this.connectTypeSwitchButton();
+    this.connectTypeSwitchButton(type);
   }
 
   connectMoreInfoButton() {}
 
-  connectTypeSwitchButton() {
+  connectTypeSwitchButton(type) {
     //STEP 3. select button with the received id
     const projectItemElement = document.getElementById(this.prjItemsIteratorId);
-    const typeSwitchButton = projectItemElement.querySelector(
+    let typeSwitchButton = projectItemElement.querySelector(
       "button:last-of-type"
     );
-    typeSwitchButton.addEventListener("click", this.updateProjectListHandler);
+
+    // STEP 9 update buttons text
+    typeSwitchButton = DOMHelper.clearEventListeners(typeSwitchButton);
+    typeSwitchButton.textContent = type === "active" ? "Finish" : "Activate";
+
+    typeSwitchButton.addEventListener(
+      "click",
+      this.updateProjectListHandler.bind(null, this.prjItemsIteratorId)
+    );
+  }
+  // STEP 8.1 update
+  update(updateProjectListFunction, type) {
+    this.updateProjectListHandler = updateProjectListFunction;
+    this.connectTypeSwitchButton(type);
   }
 }
 
@@ -30,7 +59,11 @@ class ProjectList {
     const projectItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItemsIterator of projectItems) {
       this.projects.push(
-        new ProjectItem(prjItemsIterator.id, this.switchProject.bind(this))
+        new ProjectItem(
+          prjItemsIterator.id,
+          this.switchProject.bind(this),
+          this.type
+        )
       );
     }
   }
@@ -38,15 +71,26 @@ class ProjectList {
   setSwitchHandlerFunction(switchHandlerFunction) {
     this.switchHandler = switchHandlerFunction;
   }
-
-  addProject() {
+  //   STEP 7. move items to different list
+  addProject(project) {
+    this.projects.push(project);
     console.log(this);
+    DOMHelper.moveElement(
+      project.prjItemsIteratorId,
+      `#${this.type}-projects ul`
+    );
+    //STEP 8 update switchandler when the item moved to different list
+    project.update(this.switchProject.bind(this), this.type);
   }
 
   switchProject(itemId) {
     //STEP 4. remove item from projects array and switch it to the other list
-    this.switchHandler(this.projects.find((p) => p.id === itemId)); //STEP 4.2 pass item to switchHandlerFunction
-    this.projects = this.projects.filter((p) => p.id !== itemId); //STEP 4.1 filter out item from projects array, which not returns id not equal to searched id
+    this.switchHandler(
+      this.projects.find((p) => p.prjItemsIteratorId === itemId)
+    ); //STEP 4.2 pass item to switchHandlerFunction
+    this.projects = this.projects.filter(
+      (p) => p.prjItemsIteratorId !== itemId
+    ); //STEP 4.1 filter out item from projects array, which not returns id not equal to searched id
   }
 }
 
