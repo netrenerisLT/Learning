fc1c0b3e261d924dd7a0ae2535b75e7a59cfafbf
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-16 19:36:46 +0200

    converted routes to ejs

--- 100 - NodeJS/tasks 2/app.js
+++ 100 - NodeJS/tasks 2/app.js
@@ -4,22 +4,28 @@ const fs = require("fs");
 
 const app = express();
 app.use(express.static("public")); //checks if the static file in public folder (like css, js) can be delivered to route
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); //let and read/write data to JSON
+
+app.set("views", path.join(__dirname, "views")); //we tell express where to find template files which be use bu template engine
+app.set("view engine", "ejs"); //we tell express to use "template engine" for process our view files before sending them as html
 
 const htmlFilePath = (fileName) => {
   return path.join(__dirname, "views", `${fileName}.html`);
 };
 
 app.get("/", function (req, res) {
-  res.sendFile(htmlFilePath("index"));
+  //   res.sendFile(htmlFilePath("index"));
+  res.render("index"); //parse template file with help of template engine and converts data to HTML which is sent back to the browser
 });
 
 app.get("/restaurants", function (req, res) {
-  res.sendFile(htmlFilePath("restaurants"));
+  //   res.sendFile(htmlFilePath("restaurants"));
+  res.render("restaurants");
 });
 
 app.get("/recommend", function (req, res) {
-  res.sendFile(htmlFilePath("recommend"));
+  //   res.sendFile(htmlFilePath("recommend"));
+  res.render("recommend");
 });
 
 app.post("/recommend", function (req, res) {
@@ -36,11 +42,13 @@ app.post("/recommend", function (req, res) {
 });
 
 app.get("/about", function (req, res) {
-  res.sendFile(htmlFilePath("about"));
+  //   res.sendFile(htmlFilePath("about"));
+  res.render("about");
 });
 
 app.get("/confirm", function (req, res) {
-  res.sendFile(htmlFilePath("confirm"));
+  //   res.sendFile(htmlFilePath("confirm"));
+  res.render("confirm");
 });
 
 app.listen(3000);
