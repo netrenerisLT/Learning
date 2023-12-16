d651aece8228b0b8696b295179f6e762481b5b7e
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-16 19:19:26 +0200

    added submit form

--- 100 - NodeJS/tasks 2/app.js
+++ 100 - NodeJS/tasks 2/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
+app.use(express.static("public")); //checks if the static file in public folder (like css, js) can be delivered to route
+app.use(express.urlencoded({ extended: false }));
 
 const htmlFilePath = (fileName) => {
   return path.join(__dirname, "views", `${fileName}.html`);
@@ -17,6 +20,19 @@ app.get("/restaurants", function (req, res) {
 
 app.get("/recommend", function (req, res) {
   res.sendFile(htmlFilePath("recommend"));
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body; //extract data from input form
+  const filePath = path.join(__dirname, "data", "restaurants.json"); //access the data file
+
+  const fileData = fs.readFileSync(filePath); //read data in file
+  const storedRestaurants = JSON.parse(fileData); //translate data to JS format
+
+  storedRestaurants.push(restaurant); // push data to file's array
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants)); // save this data and convert data to JSON format
+
+  res.redirect("/confirm");
 });
 
 app.get("/about", function (req, res) {
