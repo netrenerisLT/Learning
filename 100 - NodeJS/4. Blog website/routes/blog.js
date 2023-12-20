dd17ebd5025344309fd1c20d97deacb57d2ca09f
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-20 16:39:00 +0200

    added view post router

--- 100 - NodeJS/4. Blog website/routes/blog.js
+++ 100 - NodeJS/4. Blog website/routes/blog.js
@@ -32,4 +32,31 @@ router.post("/posts", async function (req, res) {
   );
   res.redirect("/posts");
 });
+
+router.get("/posts/:id", async function (req, res) {
+  const postId = req.params.id;
+  const [posts] = await db.query(
+    "SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts INNER JOIN authors ON posts.author_id = authors.id WHERE posts.id = ?",
+    [postId]
+  );
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render("404");
+  } else {
+    res.render("post-detail", { post: posts[0] });
+    console.log(posts);
+  }
+});
+
+// router.get("/update-post/:2", async function (req, res) {
+//   const postId = req.params.id;
+//   const [postInfo] = await db.query("SELECT * FROM posts");
+//   for (const iterator of postInfo) {
+//     if (iterator.id === 1) {
+//       return res.render("/update-post", { postInfo: postInfo });
+//     }
+//   }
+//   console.log(postInfo);
+//   console.log(req.params.id);
+// });
 module.exports = router;
