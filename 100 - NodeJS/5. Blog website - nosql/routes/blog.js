5a6557ddf2e977a4984145531ef1592af7488e11
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-24 11:29:16 +0200

    init

--- 100 - NodeJS/5. Blog website - nosql/routes/blog.js
+++ 100 - NodeJS/5. Blog website - nosql/routes/blog.js
@@ -1,17 +1,49 @@
-const express = require('express');
+const express = require("express");
+const mongodb = require("mongodb");
 
 const router = express.Router();
+const db = require("../data/database");
+const ObjectId = mongodb.ObjectId;
 
-router.get('/', function(req, res) {
-  res.redirect('/posts');
+router.get("/", function (req, res) {
+  res.redirect("/posts");
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list');
+router.get("/posts", async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection("posts")
+    .find({}, { title: 1, summary: 1, "author.name": 1 })
+    .toArray();
+  res.render("posts-list", { posts: posts });
 });
 
-router.get('/new-post', async function(req, res) {
-  res.render('create-post');
+router.get("/new-post", async function (req, res) {
+  const authors = await db.getDb().collection("authors").find().toArray();
+  res.render("create-post", { authors: authors });
+});
+
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+
+  const data = {
+    title: req.body.title,
+    summary: req.body.summary,
+    content: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+  const result = await db.getDb().collection("posts").insertOne(data);
+  console.log(result);
+  res.redirect("/posts");
 });
 
 module.exports = router;
