9a596759b752756a5d310d1bac2ff7da1675aed6
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-24 15:32:45 +0200

    init

--- 100 - NodeJS/5. Blog website - nosql/routes/blog.js
+++ 100 - NodeJS/5. Blog website - nosql/routes/blog.js
@@ -13,17 +13,25 @@ router.get("/posts", async function (req, res) {
   const posts = await db
     .getDb()
     .collection("posts")
-    .find({}, { title: 1, summary: 1, "author.name": 1 })
+    .find({})
+    .project({ title: 1, summary: 1, "author.name": 1 })
     .toArray();
   res.render("posts-list", { posts: posts });
 });
 
-router.get("/posts/:id", async function (req, res) {
-  const postId = req.params.id;
+router.get("/posts/:id", async function (req, res, next) {
+  let postId = req.params.id;
+
+  try {
+    postId = new ObjectId(postId);
+  } catch (error) {
+    return res.status(404).render("404");
+  }
+
   const post = await db
     .getDb()
     .collection("posts")
-    .findOne({ _id: new ObjectId(postId) }, { summary: 0 });
+    .findOne({ _id: postId }, { summary: 0 });
 
   if (!post) {
     return res.status(404).render("404");
