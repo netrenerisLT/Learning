e25aefd291a082e304a07432e420ff9aa0f69ddc
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-24 21:23:48 +0200

    uploaded image preview

--- 100 - NodeJS/6. upload files/routes/users.js
+++ 100 - NodeJS/6. upload files/routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const multer = require("multer");
 
+const db = require("../data/database");
+
 const storageMulter = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "images");
@@ -13,19 +15,24 @@ const storageMulter = multer.diskStorage({
 const router = express.Router();
 const upload = multer({ storage: storageMulter });
 
-router.get("/", function (req, res) {
-  res.render("profiles");
+router.get("/", async function (req, res) {
+  const users = await db.getDb().collection("users").find().toArray();
+  res.render("profiles", { users: users });
 });
 
 router.get("/new-user", function (req, res) {
   res.render("new-user");
 });
 
-router.post("/profiles", upload.single("image"), function (req, res) {
+router.post("/profiles", upload.single("image"), async function (req, res) {
   const uploadedImage = req.file;
   const userData = req.body;
-  console.log(uploadedImage);
-  console.log(userData);
+
+  await db.getDb().collection("users").insertOne({
+    name: userData.username,
+    imagePath: uploadedImage.path,
+  });
+
   res.redirect("/");
 });
 
