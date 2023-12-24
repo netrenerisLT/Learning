fd407ee76eb67d3eb0494fef9e5114d573e8a2a3
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-24 13:43:25 +0200

    added date

--- 100 - NodeJS/5. Blog website - nosql/routes/blog.js
+++ 100 - NodeJS/5. Blog website - nosql/routes/blog.js
@@ -18,6 +18,28 @@ router.get("/posts", async function (req, res) {
   res.render("posts-list", { posts: posts });
 });
 
+router.get("/posts/:id", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { summary: 0 });
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  post.readableDate = post.date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  post.date = post.date.toISOString();
+
+  res.render("post-detail", { post: post });
+});
+
 router.get("/new-post", async function (req, res) {
   const authors = await db.getDb().collection("authors").find().toArray();
   res.render("create-post", { authors: authors });
