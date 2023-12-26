ba4103a73f7ff28b24af23e64ce90d5f9a471abf
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-26 19:59:11 +0200

    added user creation and login

--- 100 - NodeJS/8. authentication/routes/demo.js
+++ 100 - NodeJS/8. authentication/routes/demo.js
@@ -1,29 +1,73 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcrypt");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-router.get('/signup', function (req, res) {
-  res.render('signup');
+router.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
-router.get('/login', function (req, res) {
-  res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.post('/signup', async function (req, res) {});
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData["confirm-email"];
+  const enteredPassword = userData.password;
 
-router.post('/login', async function (req, res) {});
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
 
-router.get('/admin', function (req, res) {
-  res.render('admin');
+  const userInfo = {
+    email: enteredEmail,
+    password: hashedPassword,
+  };
+
+  const result = await db.getDb().collection("users").insertOne(userInfo);
+
+  res.redirect("/login");
 });
 
-router.post('/logout', function (req, res) {});
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData["confirm-email"];
+  const enteredPassword = userData.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (!existingUser) {
+    return red.redirect("/login");
+  }
+
+  const passwordEqual = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  );
+
+  if (!passwordEqual) {
+    return res.redirect("/login");
+  }
+
+  console.log("authenticared");
+  console.log("user autheenticated");
+  res.redirect("/admin");
+});
+
+router.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {});
 
 module.exports = router;
