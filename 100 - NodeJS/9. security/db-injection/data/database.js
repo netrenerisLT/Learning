79eac60da89f33dd361909f39a36087bd646a15e
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-29 12:02:44 +0200

    SQL injection test

--- 100 - NodeJS/9. security/db-injection/data/database.js
+++ 100 - NodeJS/9. security/db-injection/data/database.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  database: 'security',
-  user: 'root',
-  password: 'your-pw',
-  multipleStatements: true
-})
+  host: "localhost",
+  database: "security",
+  user: "root",
+  password: "jonass123kK",
+  multipleStatements: true,
+});
 
 module.exports = pool;
