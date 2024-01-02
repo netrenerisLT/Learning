58f15090f20ae46073217c167278e6edccfeb8c2
Jonas Kazlauskas <netreneris@gmail.com>
2024-01-02 15:28:36 +0200

    added delete/edit MVC

---
+++ 100 - NodeJS/10. MVC & refactoring/models/post.js
@@ -0,0 +1,41 @@
+const { ObjectId } = require("mongodb");
+const db = require("../data/database");
+
+class Post {
+  constructor(title, content, id) {
+    this.title = title;
+    this.content = content;
+    this.id = id;
+
+    if (id) {
+      this.id = new ObjectId(id);
+    }
+  }
+
+  async save() {
+    let result;
+    if (this.id) {
+      result = await db
+        .getDb()
+        .collection("posts")
+        .updateOne(
+          { _id: this.id },
+          { $set: { title: this.title, content: this.content } }
+        );
+    } else {
+      result = await db.getDb().collection("posts").insertOne({
+        title: this.title,
+        content: this.content,
+      });
+    }
+    return result;
+  }
+  async delete() {
+    if (!this.id) {
+      return;
+    }
+    await db.getDb().collection("posts").deleteOne({ _id: this.id });
+  }
+}
+
+module.exports = Post;
