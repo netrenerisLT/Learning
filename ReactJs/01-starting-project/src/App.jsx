7c53bbe7271128c78dce59493a2180c4a6ce0ad9
Jonas Kazlauskas <netreneris@gmail.com>
2024-01-13 11:48:39 +0200

    splitting code

--- ReactJs/01-starting-project/src/App.jsx
+++ ReactJs/01-starting-project/src/App.jsx
@@ -1,42 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
+import { CoreConcept } from "./components/CoreConcept.jsx";
+import { Header } from "./components/Header.jsx";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = [
-  "Fundamental",
-  "Crucial",
-  "Core",
-  "Best",
-  "Legendary",
-];
-
-function genRandom(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const randomWord = reactDescriptions[genRandom(reactDescriptions.length)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {randomWord} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
