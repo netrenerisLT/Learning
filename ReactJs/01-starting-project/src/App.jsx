18cafcd218df0034d4d8bde5c02023c349bbe7f3
Jonas Kazlauskas <netreneris@gmail.com>
2024-01-15 16:38:11 +0200

    init

--- ReactJs/01-starting-project/src/App.jsx
+++ ReactJs/01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { CoreConcept } from "./components/CoreConcept.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 import { Header } from "./components/Header.jsx";
 import { TabButton } from "./components/TabButton.jsx";
 import { CORE_CONCEPTS, EXAMPLES } from "./data";
@@ -18,14 +18,17 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
           </ul>
         </section>
         <section id="examples">
