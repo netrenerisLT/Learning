fdcf0e5cb77a48b1476f1097f232f31e25c292e5
Jonas Kazlauskas <netreneris@gmail.com>
2024-01-16 11:00:06 +0200

    added name change/save

--- ReactJs/02-tic-tac-toe-project/src/App.jsx
+++ ReactJs/02-tic-tac-toe-project/src/App.jsx
@@ -5,10 +5,10 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="Set Player 1" symbol="X">
+          <Player initialName="Player 1" symbol="X">
             BB
           </Player>
-          <Player name="Set Player 2" symbol="O"></Player>
+          <Player initialName="Player 2" symbol="O"></Player>
         </ol>
         <div>GameBoard</div>
       </div>
--- ReactJs/02-tic-tac-toe-project/src/components/Player.jsx
+++ ReactJs/02-tic-tac-toe-project/src/components/Player.jsx
@@ -2,18 +2,32 @@ import { useState } from "react";
 
 export default function Player(props) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(props.initialName);
+
+  function handleClick() {
+    setIsEditing((isEditing) => !isEditing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
 
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input type="text" required />
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleChange}
+            required
+          />
         ) : (
-          <span className="player-name">{props.name} </span>
+          <span className="player-name">{playerName} </span>
         )}
         <span className="player-symbol">{props.symbol}</span>
       </span>
-      <button onClick={() => setIsEditing(true)}>Edit</button>
+      <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
