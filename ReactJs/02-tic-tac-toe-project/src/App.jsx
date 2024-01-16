82d282ce70cacc04c7bf4a57a75885e3df395f04
Jonas Kazlauskas <netreneris@gmail.com>
2024-01-16 12:18:50 +0200

    added game board with selection

--- ReactJs/02-tic-tac-toe-project/src/App.jsx
+++ ReactJs/02-tic-tac-toe-project/src/App.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  function changeActivePlayer(params) {
+    setActivePlayer((curActivePlayer) => {
+      curActivePlayer === "X" ? "O" : "X";
+    });
+  }
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player initialName="Player 1" symbol="X">
-            BB
-          </Player>
+          <Player initialName="Player 1" symbol="X"></Player>
           <Player initialName="Player 2" symbol="O"></Player>
         </ol>
-        <div>GameBoard</div>
+        <div>
+          <GameBoard />
+        </div>
       </div>
+      LOG
     </main>
   );
 }
---
+++ ReactJs/02-tic-tac-toe-project/src/components/GameBoard.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelection(rowIndex, colIndex, symbol) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelection(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
