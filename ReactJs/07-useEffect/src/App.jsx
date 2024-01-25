e232f7c2f7ec452a3a1e39dd1805c18b7e266bf2
Jonas Kazlauskas <netreneris@gmail.com>
2024-01-25 11:50:44 +0200

    init

--- ReactJs/07-useEffect/src/App.jsx
+++ ReactJs/07-useEffect/src/App.jsx
@@ -41,6 +41,14 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -76,7 +84,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="sorting places by distance"
           onSelectPlace={handleSelectPlace}
         />
       </main>
