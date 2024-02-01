2b6d44a9a5d434edbc4a6e6c7beccbad9639d8a7
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-01 11:17:38 +0200

    init

--- ReactJs/12-Forms & user input/src/components/Login.jsx
+++ ReactJs/12-Forms & user input/src/components/Login.jsx
@@ -1,6 +1,12 @@
 export default function Login() {
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log("first");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
