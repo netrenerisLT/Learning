a8482c84cbb11a56651fae2c87cb51d3be9d8160
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-01 16:06:21 +0200

    init

--- ReactJs/12-Forms & user input/src/components/Input.jsx
+++ ReactJs/12-Forms & user input/src/components/Input.jsx
@@ -1,6 +1,6 @@
 export default function input({ label, id, error, ...props }) {
   return (
-      <div className="control">
+      <div className="control no-margin">
         <label htmlFor={id}>{label}</label>
         <input id={id} {...props} />
         <div className="control-error">{error && <p>{error}</p>}</div>
