488e26bc919359857f687a74e07b871fbd0faa51
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-12 15:55:08 +0200

    init

--- ReactJs/16-Routing/src/pages/Home.js
+++ ReactJs/16-Routing/src/pages/Home.js
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateFunction() {
+    navigate("/products");
+  }
   return (
     <>
       <h1>HomePage</h1>
+      <button onClick={navigateFunction}>NavigateHandler</button>
     </>
-  )
+  );
 }
 
 export default HomePage;
