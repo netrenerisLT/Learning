7ccec8097a838430f860b74e09ca6deb11df5e17
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-12 14:40:09 +0200

    added routes

--- ReactJs/16-Routing/src/App.js
+++ ReactJs/16-Routing/src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import Products from "./pages/Products";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage></HomePage> },
+  { path: "/products", element: <Products /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
