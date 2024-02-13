47fa3e1586c97445f56a8e5431374ce3098a77ea
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-13 11:58:17 +0200

    added router loader

--- ReactJs/16.1-Routing project/frontend/src/App.js
+++ ReactJs/16.1-Routing project/frontend/src/App.js
@@ -27,30 +27,42 @@
 // DONE
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 // DONE
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EditEventPage from './pages/EditEvent';
-import EventDetailPage from './pages/EventDetail';
-import EventsPage from './pages/Events';
-import EventsRootLayout from './pages/EventsRoot';
-import HomePage from './pages/Home';
-import NewEventPage from './pages/NewEvent';
-import RootLayout from './pages/Root';
+import EditEventPage from "./pages/EditEvent";
+import EventDetailPage from "./pages/EventDetail";
+import EventsPage from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: 'events',
+        path: "events",
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <EventsPage /> },
-          { path: ':id', element: <EventDetailPage /> },
-          { path: 'new', element: <NewEventPage /> },
-          { path: ':id/edit', element: <EditEventPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: async () => {
+              const response = await fetch("http://localhost:8080/events");
+
+              if (!response.ok) {
+              } else {
+                const resData = await response.json();
+                return resData.events
+              }
+            },
+          },
+          { path: ":id", element: <EventDetailPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":id/edit", element: <EditEventPage /> },
         ],
       },
     ],
--- ReactJs/16.1-Routing project/frontend/src/pages/Events.js
+++ ReactJs/16.1-Routing project/frontend/src/pages/Events.js
@@ -1,27 +1,12 @@
-import { Link } from 'react-router-dom';
-
-const DUMMY_EVENTS = [
-  {
-    id: 'e1',
-    title: 'Some event',
-  },
-  {
-    id: 'e2',
-    title: 'Another event',
-  },
-];
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
+  const events = useLoaderData();
+
   return (
     <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   );
 }
