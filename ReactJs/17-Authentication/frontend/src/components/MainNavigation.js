d960df8add37a02d28c67e38a3c7d34e376527d8
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-16 14:27:31 +0200

    conditionally show menu items for loged in users

--- ReactJs/17-Authentication/frontend/src/components/MainNavigation.js
+++ ReactJs/17-Authentication/frontend/src/components/MainNavigation.js
@@ -1,9 +1,11 @@
-import { NavLink, Form } from "react-router-dom";
+import { NavLink, Form, useRouteLoaderData } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
+  const tokenRoot = useRouteLoaderData("root");
+
   return (
     <header className={classes.header}>
       <nav>
@@ -39,21 +41,25 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              to="/auth?mode=login"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Login
-            </NavLink>
-          </li>
-          <li>
-            <Form action="/logout" method="post">
-              <button>Logout</button>
-            </Form>
-          </li>
+          {!tokenRoot && (
+            <li>
+              <NavLink
+                to="/auth?mode=login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Login
+              </NavLink>
+            </li>
+          )}
+          {tokenRoot && (
+            <li>
+              <Form action="/logout" method="post">
+                <button>Logout</button>
+              </Form>
+            </li>
+          )}
         </ul>
       </nav>
       <NewsletterSignup />
