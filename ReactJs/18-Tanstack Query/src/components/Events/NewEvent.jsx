5361ba16a9e93d6df34b439f8af176c8371d7250
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-17 17:14:05 +0200

    sending form data to backend

--- ReactJs/18-Tanstack Query/src/components/Events/NewEvent.jsx
+++ ReactJs/18-Tanstack Query/src/components/Events/NewEvent.jsx
@@ -1,25 +1,45 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { createNewEvent } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  //dont send data when component is renderer, as it by default with useQuery
+  //mutate used when you need to tell when to send the request
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && "Submitting data..."}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="An error while submiting event."
+          message={error.info?.message || "Please try again."}
+        ></ErrorBlock>
+      )}
     </Modal>
   );
 }
