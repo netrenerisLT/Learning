3b3233cf3e5ea938971ae024349f5e6cd2036857
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-17 17:37:30 +0200

    fetching selectable images

--- ReactJs/18-Tanstack Query/src/util/http.js
+++ ReactJs/18-Tanstack Query/src/util/http.js
@@ -39,3 +39,17 @@ export async function createNewEvent(eventData) {
 
   return event;
 }
+
+export async function fetchSelectableImages({ signal }) {
+  const response = await fetch(`http://localhost:3000/events/images`, { signal });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while loading the images");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { images } = await response.json();
+  return images;
+}
