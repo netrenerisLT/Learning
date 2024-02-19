ec106b1746c5c9aa8636613304fbad2bcfbd1a02
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-19 14:43:23 +0200

    added error and 404 pages

--- ReactJs/19-NextJs/foodies-starting-project/app/meals/page.js
+++ ReactJs/19-NextJs/foodies-starting-project/app/meals/page.js
@@ -2,10 +2,15 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/MealsGrid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
-  const meals = await getMeals()
+async function Meals() {
+  const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -18,7 +23,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals}/>
+        <Suspense
+          fallback={<p className={classes.loading}>Loading meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
--- ReactJs/19-NextJs/foodies-starting-project/lib/meals.js
+++ ReactJs/19-NextJs/foodies-starting-project/lib/meals.js
@@ -2,6 +2,7 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals() {
+  
   await new Promise((resolve) => setTimeout(resolve, 2000)); //just delays loading data, because need to check loading spinner
   return db.prepare("SELECT * FROM meals").all();
 }
