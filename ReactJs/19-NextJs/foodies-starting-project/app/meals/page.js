45eb8b591fa9a1698784df440b4e46faf01f4ce3
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-19 11:52:13 +0200

    added meals page and meal component

--- ReactJs/19-NextJs/foodies-starting-project/app/meals/page.js
+++ ReactJs/19-NextJs/foodies-starting-project/app/meals/page.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/MealsGrid";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals for <span className={classes.span}>you.</span>
+        </h1>
+        <p>Choose your meal.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your meal.</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]}/>
+      </main>
+    </>
   );
 }
