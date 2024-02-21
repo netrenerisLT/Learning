f6669620eab6eff20d1418703ba058897288d25d
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-21 20:43:28 +0200

    animation on scrolling

--- ReactJs/20-animations/starting project/src/pages/Welcome.jsx
+++ ReactJs/20-animations/starting project/src/pages/Welcome.jsx
@@ -1,24 +1,46 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-import cityImg from '../assets/city.jpg';
-import heroImg from '../assets/hero.png';
+import cityImg from "../assets/city.jpg";
+import heroImg from "../assets/hero.png";
 
 export default function WelcomePage() {
+  //get information about how px scrolled in vertical
+  const { scrollY } = useScroll();
+
+  //trnsforms values, 1arg - values which should be transform (in px), 2arg - array of breakepoints (start at 0px, second 200px and etc)
+  // 3arg - array of values those breakpoints and the values between those breakpoints should be transformed to (first opacity of 1, second opacity of 0.5)
+  // after scrolling 200px vertically, value will be 0.5, between 200-300 value is 0.5, and between 300-500 value decrease to 0.2
+  const yCity = useTransform(scrollY, [0, 200], [0, -100]);
+  const yHero = useTransform(scrollY, [0, 200], [0, -150]);
+  const scaleText = useTransform(scrollY, [0, 300], [1, 1.2]);
+  const opacityCity = useTransform(
+    scrollY,
+    [0, 200, 300, 500],
+    [1, 0.5, 0.5, 0.2]
+  );
+
   return (
     <>
       <header id="welcome-header">
-        <div id="welcome-header-content">
+        <motion.div style={{ scale: scaleText }} id="welcome-header-content">
           <h1>Ready for a challenge?</h1>
           <Link id="cta-link" to="/challenges">
             Get Started
           </Link>
-        </div>
-        <img
+        </motion.div>
+        <motion.img
+          style={{ opacity: opacityCity, y: yCity }}
           src={cityImg}
           alt="A city skyline touched by sunlight"
           id="city-image"
         />
-        <img src={heroImg} alt="A superhero wearing a cape" id="hero-image" />
+        <motion.img
+          style={{ y: yHero }}
+          src={heroImg}
+          alt="A superhero wearing a cape"
+          id="hero-image"
+        />
       </header>
       <main id="welcome-content">
         <section>
