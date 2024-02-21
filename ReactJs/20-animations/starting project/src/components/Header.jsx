2209cf74c99d9d6e9474ba299956974273af48d1
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-21 18:12:10 +0200

    staggerChildren and variants

--- ReactJs/20-animations/starting project/src/components/Header.jsx
+++ ReactJs/20-animations/starting project/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import NewChallenge from "./NewChallenge.jsx";
 
@@ -22,9 +22,19 @@ export default function Header() {
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            backgroundColor: `#${Math.floor(
+              Math.random() * (999999 - 100000 + 1) + 100000
+            )}`,
+          }}
+          transition={{ type: "spring", stiffness: 100, mass: 10 }}
+          onClick={handleStartAddNewChallenge}
+          className="button"
+        >
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
