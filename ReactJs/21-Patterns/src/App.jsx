2b77687fafbc6f26189d4a2ac34439c0832bbca9
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-22 11:43:06 +0200

    added accordion

--- ReactJs/21-Patterns/src/App.jsx
+++ ReactJs/21-Patterns/src/App.jsx
@@ -1,5 +1,36 @@
+import Accordion from "./components/Accordon/Accordion";
+import AccordionItem from "./components/Accordon/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work wih us</h2>
+        <Accordion className="accordion">
+          <AccordionItem
+            id="experience"
+            className="accordion-item"
+            title="We got experience"
+          >
+            <article>
+              <p>You cant go wrong with us</p>
+              <p>We are proffesionals</p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            id="local-guides"
+            className="accordion-item"
+            title="Over 20 projects"
+          >
+            <article>
+              <p>We can prove you</p>
+              <p>Just contact us</p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  )
 }
 
 export default App;
