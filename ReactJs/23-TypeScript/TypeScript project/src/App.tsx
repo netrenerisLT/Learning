68e0bae011dff040fdd801489bd8de86acc08d09
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-26 13:15:53 +0200

    added form submit as state

--- ReactJs/23-TypeScript/TypeScript project/src/App.tsx
+++ ReactJs/23-TypeScript/TypeScript project/src/App.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [
-    new Todo("learn swim"),
-    new Todo("learn react")
-  ]
+  //usestate is generic fn and we need to tell what kind of data we need to store in state
+  //at first deafult value is empty array but then has shape as declared in Todo class
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodo) => {
+      return prevTodo.concat(newTodo);
+    });
+  };
+
   return (
-    <div >
+    <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
---
+++ ReactJs/23-TypeScript/TypeScript project/src/components/NewTodo.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  //need to specify which element will be stored to this ref and we need to add starting value (e.x.null)
+  const todoRef = useRef<HTMLInputElement>(null);
+
+  function submitForm(event: React.FormEvent) {
+    event.preventDefault();
+    // "?" is used becouse TS don't know if there wll be established a connection between submtForm and the input value from the form (cant get value, before we load the input form and get value)
+    //we can use "!" to say that the connecton is established vefore we call for ths value
+    const enteredText = todoRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  }
+
+  return (
+    <form onSubmit={submitForm}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" ref={todoRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
