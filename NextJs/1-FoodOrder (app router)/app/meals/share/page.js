f1f26b102d91915d8e5a775e711e07979814fb94
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-29 14:55:55 +0200

    added show an errror on form submit

--- NextJs/1-FoodOrder (app router)/app/meals/share/page.js
+++ NextJs/1-FoodOrder (app router)/app/meals/share/page.js
@@ -1,10 +1,12 @@
-import React from "react";
-import classes from "./page.module.css";
+"use client";
+import { useFormState } from "react-dom";
 import ImagePicker from "@/components/meals/image-picker";
+import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function MealsShare() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -14,7 +16,7 @@ export default function MealsShare() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,7 +44,8 @@ export default function MealsShare() {
               required
             ></textarea>
           </p>
-          <ImagePicker name="image" label="your image" />
+          <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
