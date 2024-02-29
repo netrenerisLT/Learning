4a7efbc5ec5d1ab398169e173375adec9bd92f55
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-29 12:37:23 +0200

    added error and not found page

---
+++ NextJs/1-FoodOrder (app router)/app/meals/errror.js
@@ -0,0 +1,9 @@
+"use client"
+export default function MealsError({error}) {
+  return (
+    <main className="error">
+      <h1>an Error</h1>
+      <p>Please try again later.</p>
+    </main>
+  );
+}
---
+++ NextJs/1-FoodOrder (app router)/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound({error}) {
+  return (
+    <main className="error">
+      <h1>Page not found.</h1>
+      <p>Please try again later.</p>
+    </main>
+  );
+}
