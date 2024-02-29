9a14372a133ba9d9483ae5e0238e8c3076df6048
Jonas Kazlauskas <netreneris@gmail.com>
2024-02-29 12:53:25 +0200

    added meal info fetch

--- NextJs/1-FoodOrder (app router)/components/meals/meals-grid.js
+++ NextJs/1-FoodOrder (app router)/components/meals/meals-grid.js
@@ -3,7 +3,6 @@ import styles from "./meals-grid.module.css";
 import MealItem from "./meal-item";
 
 export default function MealsItemGrid({ meals }) {
-    console.log(meals)
   return (
     <ul className={styles.meals}>
       {meals.map((meal) => 
--- NextJs/1-FoodOrder (app router)/lib/meals.js
+++ NextJs/1-FoodOrder (app router)/lib/meals.js
@@ -3,5 +3,8 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals() {
-        return db.prepare("SELECT * FROM meals").all();
+  return db.prepare("SELECT * FROM meals").all();
+}
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
