70df09f975b5478c4a2f899956f2c6cb16e69751
Jonas Kazlauskas <netreneris@gmail.com>
2024-04-09 20:17:54 +0300

    init

--- TypeScript/01 - Basics/src/custom/app.ts
+++ TypeScript/01 - Basics/src/custom/app.ts
@@ -1,15 +1,15 @@
-function combine(input1: number | string, input2: number | string) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
 
-const combinedAges = combine(30, 26);
-console.log(combinedAges);
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+  // while (true) {}
+}
 
-const combinedNames = combine('Max', 'Anna');
-console.log(combinedNames);
+generateError('An error occurred!', 500);
