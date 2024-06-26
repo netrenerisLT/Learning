1fd19b3bc687fee67ab81a9ad9b0ad3fd2760133
Jonas Kazlauskas <netreneris@gmail.com>
2024-06-26 13:45:44 +0300

    added flexible component

--- TypeScript/React/02 - DynamicComponents/src/App.tsx
+++ TypeScript/React/02 - DynamicComponents/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import CourseGoalList from './components/CourseGoalList.tsx';
+import Header from './components/Header.tsx';
+import NewGoal from './components/NewGoal.tsx';
+import goalsImg from './assets/goals.jpg';
+
+export type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
+export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal(goal: string, summary: string) {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: goal,
+        description: summary,
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
+  function handleDeleteGoal(id: number) {
+    setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== id));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <NewGoal onAddGoal={handleAddGoal} />
+      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
+    </main>
+  );
 }
-
-export default App
---
+++ TypeScript/React/02 - DynamicComponents/src/components/InfoBox.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from "react";
+
+type InfoBoxProps = {
+  mode: "hint" | "warning";
+  children: ReactNode;
+};
+export default function InfoBox({ mode, children }: InfoBoxProps) {
+  if (mode === "hint") {
+    return (
+      <aside className="infobox infobox-hint">
+        <p>{children}</p>
+      </aside>
+    );
+  }
+
+  return (
+    <>
+      <aside className="infobox infobox-warning warning--low">
+        <h2>Warning</h2>
+        <p>{children}</p>
+      </aside>
+    </>
+  );
+}
